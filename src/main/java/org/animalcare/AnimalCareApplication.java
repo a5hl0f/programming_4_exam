8e1e86effc5a107c029ef8ef3f636f66ac8aa11b
a5hl0f <aquib7842@gmail.com>
2023-10-04 19:12:53 +0200

    war file plugin

--- src/main/java/org/animalcare/AnimalCareApplication.java
+++ src/main/java/org/animalcare/AnimalCareApplication.java
@@ -2,9 +2,11 @@ package org.animalcare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AnimalCareApplication  extends SpringBootServletInitializer{
+public class AnimalCareApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AnimalCareApplication.class, args);
