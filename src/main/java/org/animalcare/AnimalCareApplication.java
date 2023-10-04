2d5ee51620db74410ecc672441ab2f9b5f0b5bbe
a5hl0f <79424621+a5hl0f@users.noreply.github.com>
2023-10-04 18:58:44 +0200

    Update AnimalCareApplication.java

--- src/main/java/org/animalcare/AnimalCareApplication.java
+++ src/main/java/org/animalcare/AnimalCareApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AnimalCareApplication {
+public class AnimalCareApplication  extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(AnimalCareApplication.class, args);
